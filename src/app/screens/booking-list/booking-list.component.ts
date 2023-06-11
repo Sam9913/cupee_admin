5a43069ed326cc823454b2d154411f0cf241304d
Sam9913 <sam_tan9913@hotmail.com>
2023-06-11 10:51:09 +0800

    Update booking module

--- src/app/screens/booking-list/booking-list.component.ts
+++ src/app/screens/booking-list/booking-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Booking } from 'src/app/models/booking';
+import { BookingService } from 'src/app/services/booking.service';
+import { SharedServices } from 'src/app/services/shared-services';
 
 @Component({
   selector: 'app-booking-list',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./booking-list.component.css']
 })
 export class BookingListComponent {
+  bookingList: Booking[] = [];
 
+  constructor(
+    private bookingService: BookingService, 
+    private sharedServices: SharedServices,
+    private route: ActivatedRoute
+    ) { }
+
+    ngOnInit(){
+      this.getBookingList();
+    }
+
+    getBookingList(){
+      this.sharedServices.changeLoading(true);
+      const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+      this.bookingService.getByEvent(id).subscribe(bookingList =>{
+        this.bookingList = bookingList;
+        this.sharedServices.changeLoading(false);
+      });
+    }
 }
